ff90bff7c5286d81507ffe9ec239f98a88351a24
hoangperry <prodai9573@gmail.com>
2021-12-15 01:34:56 +0700

    init flow

---
+++ yahoo_flow.py
@@ -0,0 +1,117 @@
+import yfinance as yf
+
+from pandas_datareader import data as pdr
+from datetime import datetime, timedelta
+
+import os
+import torch
+import random
+import numpy as np
+
+from ez4cast import Trainer
+from ez4cast.model.tempflow.tempflow_estimator import TempFlowEstimator
+from gluonts.dataset.multivariate_grouper import ListDataset, MultivariateGrouper
+
+import prefect
+from prefect import task, Flow
+from prefect.run_configs import LocalRun, UniversalRun
+from prefect.engine.results import LocalResult
+
+import sqlalchemy
+from prefect import task, Flow
+from prefect.storage import GitHub
+
+random.seed(42)
+np.random.seed(42)
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+torch.backends.cudnn.deterministic = True
+
+
+@task
+def fetch_data_from_yahoo():
+    ticker_dict = {
+        'AAPL': 'Apple',
+        'MSFT': 'Microsoft',
+        'GOOG': 'Google',
+        'AMZN': 'Amazon',
+        'FB': 'Facebook',
+        'NVDA': 'Nvidia',
+        'INTC': 'Intel',
+        'BABA': 'Alibaba',
+        'TSLA': 'Tesla',
+        '^GSPC': 'S&P_500',
+        '^IXIC': 'Nasdaq',
+        '^DJI': 'Dow_Jones',
+    }
+
+    data_yahoo = None
+    get_col = 'Close'
+    yf.pdr_override()
+    past_14_days = datetime.now() - timedelta(days=14)
+
+    for index_ticker, ticker in enumerate(ticker_dict):
+        ticker_data = pdr.get_data_yahoo(ticker, period="max", interval = "5m", start=past_14_days.strftime('%Y-%m-%d'))
+        if index_ticker == 0:
+            data_yahoo = ticker_data[[get_col]].copy()
+            data_yahoo.columns = [ticker]
+            continue
+        data_yahoo[ticker] = ticker_data[get_col].copy()
+
+    data_yahoo.drop(data_yahoo.tail(1).index,inplace=True)
+    data_yahoo.index = data_yahoo.index.tz_localize(None)
+    data_yahoo.index[0]
+    data_yahoo = data_yahoo[:-1]
+    return data_yahoo
+
+
+@task
+def train_model(input_df):
+    col_len = input_df.columns.__len__()
+    _freq = '5M'
+    train_data_by_c = ListDataset(
+        [{
+            'start': input_df.index[0],
+            'target': input_df[cols],
+        } for cols in input_df.columns],
+        freq=_freq,
+        one_dim_target=True
+    )
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    train_grouper = MultivariateGrouper(col_len)
+    dataset_train_by_c = train_grouper(train_data_by_c)
+    model_ts = TempFlowEstimator(
+        input_size=38,
+        freq=_freq,
+        prediction_length=1,
+        context_length=2,
+        flow_type='RealNVP',
+        target_dim=col_len,
+        trainer=Trainer(epochs=10, device=device, learning_rate=1e-3, batch_size=32),
+    )
+
+    ptd = model_ts.train_model(
+        training_data=dataset_train_by_c, input_net=None
+    )
+    list_output = list()
+    output = list(ptd.predictor.predict(dataset_train_by_c, 1000))[0].samples
+    for i in output.T:
+        list_output.append(
+            np.mean(i[0])
+        )
+    logger.info(f"Predict value: {list_output}")
+    return list_output
+
+
+with Flow(
+        "the_first_revenue_prediction",
+        result=LocalResult(),
+        storage=GitHub(
+            repo="hoangperry/prefect-github-storage-test",
+            path="duy-flow.py",
+        ),
+        run_config=UniversalRun(labels=["mail.kernel.vn"]),
+    ) as flow:
+    df = fetch_data_from_yahoo()
+    train_model(df)
+
